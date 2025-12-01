03d81ade61d6f2382b45cc08d20ec05ef58029e8
lazar <lazaraleksic994@gmail.com>
2025-12-01 12:52:52 +0100

    Tipovi podatka. Konverzija proširavanjem/skraćivanjem. ASCII konverzije char -> int i suprotno

---
+++ src/main/java/com/academy/three/CharacterDemo.java
@@ -0,0 +1,20 @@
+package com.academy.three;
+// tip.podatka varijabla= vrednost
+
+
+
+public class CharacterDemo {
+    public static void main(String[] args) {
+        char slovo = 's';
+        System.out.println("slovo= " +slovo);
+        char slovo2 = 65; // A
+        System.out.println("slovo2= " +slovo2);
+
+        int number=slovo2;
+        System.out.println("number= " +number
+
+        );
+
+
+    }
+}
---
+++ src/main/java/com/academy/three/Decimaldemo.java
@@ -0,0 +1,14 @@
+package com.academy.three;
+
+public class Decimaldemo {
+    public static void main(String[] args) {
+        /**
+         */
+        double broj1=0.3;
+        double broj2 =0.2;
+        double zbir =broj1+ broj2;
+        System.out.println("Da li je zbir=0.5? Odgovor" +(zbir== 0.5));
+        System.out.println("zbir= "+zbir);
+
+    }
+}
