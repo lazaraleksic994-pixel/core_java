0181f2c3789136c59cdb6de3a6655f3024c14afa
lazar <lazaraleksic994@gmail.com>
2025-12-02 13:09:35 +0100

    Aritmetički operatori/Unarni operatori /String vs/ concatenation

---
+++ src/main/java/com/academy/three/UnaryDemo.java
@@ -0,0 +1,18 @@
+package com.academy.three;
+
+public class UnaryDemo {
+    public static void main(String[] args) {
+        int number=23;
+        System.out.println(number++); //1. Pošalji u konzolu (23) -> nakon slanja u konzolu uvećaj za 1 -> 24
+        System.out.println(++number); //1. Uvećaj za 1 = 25 2. Pošalji u konzolu 25
+
+        int broj= 24;
+        System.out.println(broj++); // 24 -> 23
+        System.out.println(++broj); // 22 -> 22
+
+
+        boolean success= true;
+        System.out.println(!success);
+
+    }
+}
